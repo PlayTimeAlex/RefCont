5e545862647374804d6dfe6001ae9f6ff9270405
Alex <alex619@bigmir.net>
2014-07-30 19:14:53 +0300

    New pages

--- sourse/js/zz_custom.js
+++ sourse/js/zz_custom.js
@@ -1,7 +1,29 @@
 (function($) {
     $(document).ready(function(){
-        $('.b-filter__select').styler({
+        $('.b-filter__select, .pr-select').styler({
             selectSearch: false
+        });
+
+        $('select.pr-select_link').change(function() {
+            window.location = $("option:selected", this).val();
+        });
+
+        //gallery popup
+        $('.galimg').colorbox({
+            close: ""
+        });
+
+        //top nav
+        $('.parent', '.b-header__nav').hover(function(){
+           $('.sub-menu',this).css('display', 'block').stop().animate({
+               opacity:1
+           }, 300);
+        }, function(){
+            $('.sub-menu',this).stop().animate({
+                opacity:0
+            }, 300, function(){
+                $(this).css('display', 'none');
+            });
         });
     });
 
