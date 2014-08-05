0cc04a78eee43ca623419590c5c0745468777a01
Alex <alex619@bigmir.net>
2014-08-05 21:33:46 +0300

    New pages, fixes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ RefCont
 =======
 
 http://p131.createme.tk/
+
+Социконки в шапке (пока убраны <span class="b-header__social b-header__topi">Мы в соц.сетях : <a target="_blank" class="link" href="#fb"></a><a target="_blank" class="link vk" href="#vk"></a><a target="_blank" class="link twitter" href="#twitter"></a></span>)
--- sourse/js/zz_custom.js
+++ sourse/js/zz_custom.js
@@ -12,7 +12,6 @@
         $('.galimg').colorbox({
             close: ""
         });
-
         //top nav
         $('.parent', '.b-header__nav').hover(function(){
            $('.sub-menu',this).css('display', 'block').stop().animate({
@@ -37,6 +36,21 @@
                 $('.b-spoiler__content', container).stop().slideDown('fast');
             }
         });
+
+        //more parameters
+        $('.more', '.b-itemin__tech').click(function(){
+            var item = $(this).siblings('.b-itemin__techmore');
+            if(item.hasClass('open')){
+                $(this).removeClass('open').find('.text').html($(this).data('close'));
+                item.stop().slideUp('fast').removeClass('open');
+            } else {
+                $(this).addClass('open').find('.text').html($(this).data('open'));
+                item.stop().slideDown('fast').addClass('open');
+            }
+        });
+
+        //Маска для полей ввода телефона
+        $(".b-form__text_tel").mask("(999) 999-9999");
     });
 
     $(window).load(function() {
@@ -45,16 +59,42 @@
             controlNav: false,
             pauseOnHover: true
         });
-        $('.b-containers__catalog').carousel('#p', '#n', 110);
-        /*$('.b-containers__catalog').flexslider({
+
+        $('.b-itemin__slidernav').flexslider({
             animation: "slide",
             controlNav: false,
-            pauseOnHover: true,
-            itemWidth: 100,
-            itemMargin: 10,
-            move: 1
+            animationLoop: false,
+            slideshow: false,
+            itemWidth: 80,
+            itemMargin: 0,
+            asNavFor: '.b-itemin__slider',
+            minItems: 4,
+            maxItems: 4
         });
-*/
+
+        $('.b-itemin__slider').flexslider({
+            animation: "slide",
+            controlNav: false,
+            directionNav: false,
+            slideshow: false,
+            animationLoop: false,
+            sync: ".b-itemin__slidernav"
+        });
+
+        $('.b-containers__catalog').carousel('#p', '#n', 110);
+
+        $('.b-containers__catalog').each(function(){
+            var els = $('.contgal',this);
+            var group = $(this).data('group');
+            console.log('test')
+            els.colorbox({
+                close: "",
+                rel: group,
+                previous: "",
+                next: "",
+                className: "b-containers__popup"
+            });
+        });
 
     });
 }(jQuery));
